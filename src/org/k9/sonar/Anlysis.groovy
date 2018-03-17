4d0639c01fd8ea08608288b31c31167424a87477
nitinneet23 <35996827+nitinneet23@users.noreply.github.com>
2018-03-17 16:42:05 +0530

    Create Anlysis.groovy

---
+++ src/org/k9/sonar/Anlysis.groovy
@@ -0,0 +1,24 @@
+package org.k9.sonar
+import org.k9.*
+
+class Analysis {
+  def config
+  def script
+
+  Analysis(script,config) {
+      this.config = config
+      this.script = script
+  }
+  
+  void checkout(){   
+      this.script.stage ('git checkout') {
+      this.script.git(url:this.config.giturl, branch:this.config.branch)
+    }
+  }
+
+void codeanalysis(){   
+  this.script.stage ('SonarQube Analysis') {
+    this.script.sh "mvn clean package sonar:sonar"
+    }
+}
+}
