99b9f06a7d340d181055392e26c09708ce6800f5
nitinneet23 <35996827+nitinneet23@users.noreply.github.com>
2018-03-18 02:08:21 +0530

    Update Analysis.groovy

--- src/org/k9/sonar/Analysis.groovy
+++ src/org/k9/sonar/Analysis.groovy
@@ -18,16 +18,7 @@ class Analysis {
 
 void codeanalysis(){   
   this.script.stage ('SonarQube Analysis') {
-    this.script.sh "mvn clean package sonar:sonar" + 
-            '-f all/pom.xml ' +
-            '-Dsonar.projectKey=com.huettermann:all:master ' +
-            '-Dsonar.login=$SONAR_UN ' +
-            '-Dsonar.password=$SONAR_PW ' +
-            '-Dsonar.language=java ' +
-            '-Dsonar.sources=. ' +
-            '-Dsonar.tests=. ' +
-            '-Dsonar.test.inclusions=**/*Test*/** ' +
-            '-Dsonar.exclusions=**/*Test*/**'
+    this.script.sh "mvn clean package sonar:sonar"
 }
 }
 }
